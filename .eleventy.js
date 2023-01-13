d6cfc22e2b3822267fbaef398fc32e51d0ec87f3
Quasar Kim <vhfflrhs128@gmail.com>
2023-01-13 13:44:46 +0900

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = eleventyConfig => {
+    eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
+
+    eleventyConfig.addPassthroughCopy('src/*.css')
+
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            data: '_data',
+            output: '_site'
+        }
+    }
+}
