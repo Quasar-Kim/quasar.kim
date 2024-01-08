72e78cc22947c6649a4e3e79dfb8bcb9ca6555f4
Quasar-Kim <vhfflrhs128@gmail.com>
2024-01-09 04:39:10 +0900

    sitemap lastmod formatting

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,8 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy('src/robots.txt')
     eleventyConfig.addPassthroughCopy('src/favicon.ico')
 
+    eleventyConfig.addFilter("yyyymmdd", date => date.toISOString().split('T')[0])
+
     return {
         passthroughFileCopy: true,
         dir: {
